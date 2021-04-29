7b56512c9098b20bdf2c791029152a32231ea5d1
Jacob Grooms <grooms.jacob@gmail.com>
2021-04-28 20:06:22 -0400

    setting up sequelize

---
+++ config/database.js
@@ -0,0 +1,26 @@
+// Destructure database information
+const {db: {username, password, database, host}} = require('./index');
+
+module.exports = {
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: "postgres",
+        seederStorage: "sequelize",
+    },
+
+    // production database options
+    production: {
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        seederStorage: "sequelize",
+        dialectOptions : {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+};
--- config/index.js
+++ config/index.js
@@ -1,4 +1,13 @@
 module.exports = {
+    // environment
     environment: process.env.NODE_ENV || "development",
     port: process.env.PORT || 5000,
+
+    // database credentials for sequelize
+    db: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+    }
 }
